ec4d70028e8c4f979fe7e976e4ef365c568eebb8
Suman Samanta <sumansamanta.pku@gmail.com>
2026-04-30 18:23:29 +0530

    some minor fixes

--- server/server.js
+++ server/server.js
@@ -52,9 +52,11 @@ app.use("/api/messages", messageRouter)
 await connectDB();
 
 
-if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=> console.log("Server is running on PORT : " + PORT ))};
+
+server.listen(PORT, () => {
+  console.log("Server is running on PORT: " + PORT);
+});
 
 // Export server for vercel
 export default server;
