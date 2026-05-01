8ba58d7ea8614c1fffd4f4441f9c2c38c6d0c768
Suman Samanta <sumansamanta.pku@gmail.com>
2026-05-01 17:56:20 +0530

    some minor fixings

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -86,7 +86,7 @@ useEffect(() => {
       {/* bottom area */}
       <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
         <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
-          <input onChange={(e)=> setInput(e.target.value)} value={input} onKeyDown={(e)=> e.key === "Enter" ? handleSendMessage(e) : null} type="text" placeholder="Send a message" className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-gray placeholder-gray-400' />
+          <input onChange={(e)=> setInput(e.target.value)} value={input} onKeyDown={(e)=> e.key === "Enter" ? handleSendMessage(e) : null} type="text" placeholder="Send a message" className='flex-1 text-sm p-3 border-none rounded-lg outline-none bg-transparent text-white placeholder-gray-400' />
           <input onChange={handleSendImage} type="file" id='image' accept='image/png , image/jpeg' hidden />
           <label htmlFor="image">
             <img src={assets.gallery_icon} alt=""  className='w-5 mr-2 cursor-pointer'/>
